d2300d6623453c3de7e209e4a2e9656293471c40
AndyChen <shinningstar1001@gmail.com>
2017-11-23 02:31:31 +0800

    fix typo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.onEvent(async context => {
 });
 
 const server = createServer(bot);
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 server.listen(port, () => {
   console.log(`server is running on ${port} port...`);
 });
