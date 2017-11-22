7e28869d211f54ed4e26952dea0d3f12905f5d46
AndyChen <shinningstar1001@gmail.com>
2017-11-23 02:46:20 +0800

    enhace bot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { LineBot } = require('bottender');
+const { LineBot, LineHandler } = require('bottender');
 const { createServer } = require('bottender/express');
 
 const bot = new LineBot({
@@ -6,12 +6,21 @@ const bot = new LineBot({
   accessToken: 'Ft5RMCpPOP2yPm/QjgJckK6e5pUYIGnZZluswO1dkB1JPze7ZZMOE0wdITOZX7rsPRBeke6HRl05vwMYEKvjIwQwI9IwMq2ERuFePZi/PbWALV2RYVycucwj/jv5EObL0HwKFzb8I/Q8uKm5i9JAnQdB04t89/1O/w1cDnyilFU=',
 });
 
-bot.onEvent(async context => {
-  await context.sendText('Hello World');
-});
+const handler = new LineHandler()
+  .onText(/yo/i, async context => {
+    await context.sendText('Hi there!');
+  })
+  .onEvent(async context => {
+    await context.sendText("I don't know what you say.");
+  })
+  .onError(async context => {
+    await context.sendText('Something wrong happened.');
+  });
+
+bot.onEvent(handler);
 
 const server = createServer(bot);
-const port = process.env.PORT || 5000
+const port = process.env.port || 5000
 server.listen(port, () => {
   console.log(`server is running on ${port} port...`);
 });
